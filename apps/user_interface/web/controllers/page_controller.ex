ac3b834f80c94e39012549c0effe76a229e3e924
Herman verschooten <Herman@verschooten.net>
2016-09-17 19:39:35 +0200

    add bourbon/neat

--- apps/user_interface/web/controllers/page_controller.ex
+++ apps/user_interface/web/controllers/page_controller.ex
@@ -1,7 +1,30 @@
+defmodule Button do
+  defstruct name: "VRIJ", relay: 0, status: :off
+end
 defmodule UserInterface.PageController do
   use UserInterface.Web, :controller
+  alias Unipi.Relay
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", relays: get_relay_statuses
+  end
+
+  def toggle(conn, %{ "button" => button}) do
+    {relay, _rest} = Integer.parse(button)
+    Relay.toggle(relay)
+    redirect(conn, to: page_path(conn, :index))
+  end
+
+  defp get_relay_statuses do
+    [
+      %Button{name: "Relay 1", relay: 1, status: Relay.state(1)},
+      %Button{name: "Relay 2", relay: 2, status: Relay.state(2)},
+      %Button{name: "Relay 3", relay: 3, status: Relay.state(3)},
+      %Button{name: "Relay 4", relay: 4, status: Relay.state(4)},
+      %Button{name: "Relay 5", relay: 5, status: Relay.state(5)},
+      %Button{name: "Relay 6", relay: 6, status: Relay.state(6)},
+      %Button{name: "Relay 7", relay: 7, status: Relay.state(7)},
+      %Button{name: "Relay 8", relay: 8, status: Relay.state(8)}
+    ]
   end
 end
--- apps/user_interface/web/router.ex
+++ apps/user_interface/web/router.ex
@@ -17,6 +17,7 @@ defmodule UserInterface.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/toggle/:button", PageController, :toggle
   end
 
   # Other scopes may use custom stacks.
