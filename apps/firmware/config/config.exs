bc1795c6a1153157660acd4097a8825dff31e977
Herman verschooten <Herman@verschooten.net>
2016-09-17 17:19:22 +0200

    Make phoenix work

--- apps/firmware/config/config.exs
+++ apps/firmware/config/config.exs
@@ -5,8 +5,14 @@
 # is restricted to this project.
 use Mix.Config
 
-# Import target specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
-# Uncomment to use target specific configurations
+config :user_interface, UserInterface.Endpoint,
+  http: [port: 80],
+  url: [host: "localhost", port: 80],
+  root: Path.dirname(__DIR__),
+  render_errors: [accepts: ~w(html json)],
+  server: true,
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: "w/UQt/HdDpkYTMdk5uwV3kiYqET7GH0KVU6nmCACm6cIvoajaHCKk4CJPHjb4k2W"
 
-# import_config "#{Mix.Project.config[:target]}.exs"
+config :logger, level: :debug
+
