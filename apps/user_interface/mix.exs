6025f739f735baa0da694626113f47023157c496
Herman verschooten <Herman@verschooten.net>
2016-09-17 19:43:09 +0200

    Add Unipi

--- apps/user_interface/mix.exs
+++ apps/user_interface/mix.exs
@@ -21,7 +21,7 @@ defmodule UserInterface.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {UserInterface, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext]]
+     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext, :unipi]]
   end
 
   # Specifies which paths to compile per environment.
@@ -37,6 +37,7 @@ defmodule UserInterface.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:unipi, in_umbrella: true}]
   end
 end
