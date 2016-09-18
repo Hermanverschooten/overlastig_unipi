50fcd28ea4e5a3a191236fbd64a3ed70e880c270
Herman verschooten <Herman@verschooten.net>
2016-09-18 09:10:58 +0200

    Replace link with channel push

--- apps/user_interface/web/channels/relay_changes_channel.ex
+++ apps/user_interface/web/channels/relay_changes_channel.ex
@@ -1,12 +1,24 @@
 defmodule UserInterface.RelayChangesChannel do
   use UserInterface.Web, :channel
+  alias Unipi.Relay
 
-  def join("relay_changes:lobby", payload, socket) do
+  def join("relay_changes:lobby", _payload, socket) do
     {:ok, socket}
   end
 
   def join(_, _payload, socket) do
     {:ok, socket}
+  end
+
+  def handle_in("toggle", %{"button" => button}, socket) do
+    {relay, _rest} = Integer.parse(button)
+    Relay.toggle(relay)
+    broadcast! socket, "change", %{ relay: relay, state: Relay.state(relay) }
+    {:noreply, socket}
+  end
+
+  def handle_in(_cmd, _payload, socket) do
+    {:noreply, socket}
   end
 
 end
--- apps/user_interface/web/static/js/relay_changes.js
+++ apps/user_interface/web/static/js/relay_changes.js
@@ -2,7 +2,14 @@ import socket from './socket'
 
 $(
   function() {
+    // socket.logger = (kind, msg, data) => { console.log('${kind}: ${msg}', data)};
     let channel = socket.channel("relay_changes:lobby", {});
+
+    $('#button-list a').on("click", event => {
+      channel.push("toggle", { button: event.target.id });
+      console.log("push toggle for " + event.target.id);
+      });
+
     channel.join()
       .receive("ok", data => {
         console.log("Joined topic");
@@ -15,6 +22,6 @@ $(
       $('#' + changes.relay)
         .removeClass('on off')
         .addClass(changes.state)
-      })
+      });
   }
 )
