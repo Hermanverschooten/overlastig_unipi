1e3f561aecdb8d27f6adee811c8c58a28d4323e7
Herman verschooten <Herman@verschooten.net>
2016-09-18 19:55:09 +0200

    Use a callback function instead of sending a message

--- apps/unipi/lib/unipi/pins.ex
+++ apps/unipi/lib/unipi/pins.ex
@@ -5,21 +5,17 @@ defmodule Unipi.Pins do
   @doc """
   Register to be notified of events on the 12 GPIO pins of the UniPi.
 
-  Unipi.Pins.start_link [1,2,10], :falling
+  Unipi.Pins.start_link [1,2,10], :falling, fn(pin, _direction) -> IO.puts pin end
 
-  Will notify us of the :falling event for pins 1,2 and 10.
+  It will call the function for the :falling event for pins 1,2 and 10.
   You can also register for :rising, or :both
 
-  The message you receive will be:
-  {:pin_state, pin, direction}
-
-  e.g. {:pin_state, 1, :falling}
   """
 
   @gpios [4, 17, 27, 23, 22, 24, 11, 7, 8 , 9, 25, 10]
 
-  def start_link(pins, direction) do
-    GenServer.start_link(__MODULE__, %{pins: pins, callback: self, direction: direction},  name: __MODULE__)
+  def start_link(pins, direction, callback) do
+    GenServer.start_link(__MODULE__, %{pins: pins, callback: callback, direction: direction},  name: __MODULE__)
   end
 
   def init(%{pins: pins, callback: _callback, direction: direction} = opts) do
@@ -34,17 +30,14 @@ defmodule Unipi.Pins do
   defp get_gpio(pin), do: Enum.at(@gpios, pin - 1)
 
   def handle_info({:gpio_interrupt, gpio, direction}, state) do
-    Logger.info "Received Message '#{direction}' from #{gpio}"
+    pin = get_pin(gpio)
+    Logger.info "Received Message '#{direction}' from #{pin}"
     case state.direction do
-      :both -> send_msg(state.callback, gpio, direction)
-      ^direction -> send_msg(state.callback, gpio, direction)
+      :both -> state.callback.(pin, direction)
+      ^direction -> state.callback.(pin, direction)
       _ -> nil
     end
     {:noreply, state}
-  end
-
-  defp send_msg(to, gpio, direction) do
-    Kernel.send to, {:pin_state, get_pin(gpio), direction }
   end
 
   defp get_pin(gpio) do
