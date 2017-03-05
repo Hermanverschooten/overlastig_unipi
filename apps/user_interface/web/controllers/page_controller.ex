f17cc7db8c9c1e5a3acdd37633ea7772594ea894
Herman verschooten <Herman@verschooten.net>
2017-03-05 08:12:10 +0100

    Personalised buttons

--- apps/user_interface/web/controllers/page_controller.ex
+++ apps/user_interface/web/controllers/page_controller.ex
@@ -11,14 +11,14 @@ defmodule UserInterface.PageController do
 
   defp get_relay_statuses do
     [
-      %Button{name: "Relay 1", relay: 1, status: Relay.state(1)},
-      %Button{name: "Relay 2", relay: 2, status: Relay.state(2)},
-      %Button{name: "Relay 3", relay: 3, status: Relay.state(3)},
-      %Button{name: "Relay 4", relay: 4, status: Relay.state(4)},
-      %Button{name: "Relay 5", relay: 5, status: Relay.state(5)},
-      %Button{name: "Relay 6", relay: 6, status: Relay.state(6)},
-      %Button{name: "Relay 7", relay: 7, status: Relay.state(7)},
-      %Button{name: "Relay 8", relay: 8, status: Relay.state(8)}
+      %Button{name: "Strobo achter", relay: 1, status: Relay.state(1)},
+      %Button{name: "Strobo voor", relay: 2, status: Relay.state(2)},
+      %Button{name: "Rookmachine", relay: 3, status: Relay.state(3)},
+      %Button{name: "Disco-ruimte", relay: 4, status: Relay.state(4)},
+      %Button{name: "Verlichting", relay: 5, status: Relay.state(5)},
+      %Button{name: "Blazer", relay: 6, status: Relay.state(6)},
+      # %Button{name: "Relay 7", relay: 7, status: Relay.state(7)},
+      # %Button{name: "Relay 8", relay: 8, status: Relay.state(8)}
     ]
   end
 end
